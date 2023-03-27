2f5aa18818c9faf52f80fee14045aeea747cf20a
Alexey Zarubin <alken1t15@gmail.com>
2023-03-27 23:45:47 +0600

    version 5

--- src/main/java/alken1t/shop/controller/DataController.java
+++ src/main/java/alken1t/shop/controller/DataController.java
@@ -11,11 +11,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,7 +29,7 @@ public class DataController {
     private ProductRepository productRepository;
 
     @GetMapping(path = "/first_resource")
-    public Object firstResource(){
+    public Object firstResource() {
 //        TypedQuery<Product> query = entityManager.createQuery("select p from Product p", Product.class);
 //        System.out.println(query.getResultList());
 //        return query.getResultList();
@@ -41,35 +40,68 @@ public class DataController {
 //        Category category = categoryRepository.findById(1L).orElseThrow();
 //        return category;
         Optional<Category> optional = categoryRepository.findById(5L);
-        if(optional.isPresent()){
+        if (optional.isPresent()) {
             return optional.get().getName();
-        }
-        else {
+        } else {
             return "Category ont found";
         }
     }
 
-    @GetMapping(path = "/secondResource",produces = "application/json; charset=utf-8")
-    public List<String> secondResource(){
+    @GetMapping(path = "/secondResource", produces = "application/json; charset=utf-8")
+    public List<String> secondResource() {
         Sort sort = Sort.by(
                 Sort.Order.asc("category.name"),
                 Sort.Order.desc("price")
         );
         List<Product> products = productRepository.findAll(sort);
-       return products
-               .stream()
-               .map(product -> product.getName() + " ("+ product.getPrice() + ") ")
-               .toList();
+        return mapProductToString(products);
     }
 
-    @GetMapping(path = "/thirdResource",produces = "application/json; charset=utf-8")
-    public List<String> thirdResource(@RequestParam(required = false) Integer page){
-        Pageable pageable = PageRequest.of(page-1,3);
+    private static final int PAGE_SIZE = 2;
+
+    @GetMapping(path = "/thirdResource", produces = "application/json; charset=utf-8")
+    public List<String> thirdResource(@RequestParam(required = false) Integer page) {
+
+        Sort sort = Sort.by(
+                Sort.Order.desc("price")
+        );
+
+        if (page == null) {
+            return mapProductToString(productRepository.findAll(sort));
+        }
+        Pageable pageable = PageRequest.of(page - 1, PAGE_SIZE, sort);
         Page<Product> productPage = productRepository.findAll(pageable);
         List<Product> products = productPage.getContent();
+        return mapProductToString(products);
+    }
+
+    @GetMapping(path = "/forth_resource")
+    public List<String> fourthResource() {
+        //  List<Product> products = productRepository.findAllByPriceBefore(150_000);
+        // List<Product> products = productRepository.findAllByPriceAfter(100_000);
+        // List<Product> products = productRepository.findAllByPriceIsBetween(100_000,200_000);
+        //   List<Product> products = productRepository.findAllByCategoryName("Смартфоны");
+        //List<Product> products = productRepository.findAllByCategoryNameAndPriceBetween("Наушники",100_000,200_000);
+        List<Product> products = productRepository.findAllByCategoryAndPrice("Наушники", 100_000, 200_000);
+        return mapProductToString(products);
+    }
+
+    @GetMapping(path = "/image_resource", produces = "image/jpeg")
+    public byte[] imageResource() throws IOException {
+        String filePath = "/Users/ylix9g/Downloads/image.jpg";
+        FileInputStream inputStream = new FileInputStream(filePath);
+        return inputStream.readAllBytes();
+    }
+
+    @ExceptionHandler(IOException.class)
+    public String exceptionHandler(IOException e) {
+        return "File not found";
+    }
+
+    private List<String> mapProductToString(List<Product> products) {
         return products
                 .stream()
-                .map(product -> product.getName() + " ("+ product.getPrice() + ") ")
+                .map(product -> product.getName() + " (" + product.getPrice() + ") ")
                 .toList();
     }
 
